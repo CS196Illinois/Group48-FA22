0b2054d89657f5e76c967f45c152c0ed2fa6054a
Sophia Liu <sophialiu7436@gmail.com>
2022-11-16 22:25:02 -0600

    Added quiz app test
    Added quiz app test

---
+++ Project/quiz-app/lib/data/questions_list.dart
@@ -0,0 +1,72 @@
+import 'package:quiz_app_test/model/question_model.dart';
+
+List<QuestionModel> questions = [
+  QuestionModel(
+    "The Nile River Valley supported settlements. Which of the following statements confirms this?",
+    {
+      "The geography supported various desert plants and birds": false,
+      "The geography helped to unify the country": false,
+      "The geography included areas for farming": false,
+      "All of the above": true,
+    },
+  ),
+  QuestionModel("Pyramids are...", {
+    "Warehouses where surplus food is stored": false,
+    "Divine temples for people to pray in": false,
+    "Stone tombs with sides joined by a limestone roof": false,
+    "Royal tombs with four triangle-shaped sides that meet in a point on top":
+        true,
+  }),
+  QuestionModel("Which of the following best defines dynasty?", {
+    "A collection of queens": false,
+    "A collection of kingdoms": false,
+    "Series of rulers from different families": false,
+    "Series of rulers from the same family": true,
+  }),
+  QuestionModel("What event made it possible for Egyptians to grow crops?", {
+    "A heavy yearly rainfall": false,
+    "Fertile soil": false,
+    "The Nile flooded its banks yearly": true,
+    "Agricultural advances like the plow": false,
+  }),
+  QuestionModel(
+      "Which of the following beliefs was central to Egypt's religion?", {
+    "The afterlife": true,
+    "The pyramids": false,
+    "The pharaohs": false,
+    "The heavens": false,
+  }),
+  QuestionModel(
+      "Which statement best describes the burial practices of Egyptians?", {
+    "Egyptians preserved bodies as mummies": true,
+    "Egyptians carried their dead to nearby countries": false,
+    "Egyptians used a funeral pyre for their dead": false,
+    "Egyptians buried their dead in the river": false,
+  }),
+  QuestionModel(
+      "Why were tombs filled with art, jewelry, and other treasures?", {
+    "The tombs served as museums": false,
+    "The tombs were the private storage rooms of the pharaoh": false,
+    "Egyptians believed tombs to be the safest places in the kingdom": false,
+    "Egyptians believed the dead enjoyed such materials in the afterlife": true,
+  }),
+  QuestionModel("Why was the Rosetta Stone important?", {
+    "It is a unique artifact": false,
+    "It had Greek, Cuneiform, and Egyptian writing on it": false,
+    "It led to the decoding of Hieroglyphics": true,
+    "It represented peace": false,
+  }),
+  QuestionModel("What was the purpose of pyramids in Ancient Egypt?", {
+    "To protect the body of the pharaoh to help it reach afterlife": true,
+    "To create a stairway from earth to heaven": false,
+    "To create a memorial for the pharaoh": false,
+    "To store riches": false,
+  }),
+  QuestionModel(
+      "The type of government in which rule is based on religious authority", {
+    "Polytheism": false,
+    "Theocracy": true,
+    "Democracy": false,
+    "Monarchy": false,
+  }),
+];
---
+++ Project/quiz-app/lib/main.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_test/data/questions_list.dart';
+import 'package:quiz_app_test/screens/result_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // STEP 1: We need to create the folder structure of the app
+  // STEP 2: Then we need to create the model for our Quiz question
+  // STEP 3: And we need a simple list of questions to display
+  Color mainColor = Color(0xFF252c4a);
+  Color secondColor = Color(0xFF117eeb);
+  // Create a page controller for our pageview
+  PageController? _controller = PageController(initialPage: 0);
+  // Setting the game variable
+  bool isPressed = false;
+  Color isTrue = Colors.green;
+  Color isWrong = Colors.red;
+  Color btnColor = Color(0xFF117eeb);
+  int score = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Padding(
+        padding: EdgeInsets.all(18.0),
+        child: PageView.builder(
+          // Now we need to disable the scrolling so we can navigate only with the button
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _controller!,
+          onPageChanged: (page) {
+            setState(() {
+              isPressed = false;
+            });
+          },
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Question ${index + 1}/${questions.length}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 28.0,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.white,
+                  height: 8.0,
+                  thickness: 1.0,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  questions[index].question!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                for (int i = 0; i < questions[index].answer!.length; i++)
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: 18.0),
+                    child: MaterialButton(
+                      shape: StadiumBorder(),
+                      color: isPressed
+                          ? questions[index].answer!.entries.toList()[i].value
+                              ? isTrue
+                              : isWrong
+                          : secondColor,
+                      padding: EdgeInsets.symmetric(vertical: 18.0),
+                      onPressed: isPressed
+                          ? () {}
+                          : () {
+                              setState(() {
+                                isPressed = true;
+                              });
+                              if (questions[index]
+                                  .answer!
+                                  .entries
+                                  .toList()[i]
+                                  .value) {
+                                score += 10;
+                                print(score);
+                              }
+                            },
+                      child: Text(
+                        questions[index].answer!.keys.toList()[i],
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(
+                      onPressed: isPressed
+                          ? index + 1 == questions.length
+                              ? () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ResultScreen(score)));
+                                }
+                              : () {
+                                  _controller!.nextPage(
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.linear);
+                                  setState(() {
+                                    isPressed = false;
+                                  });
+                                }
+                          : null,
+                      style: OutlinedButton.styleFrom(
+                        shape: StadiumBorder(),
+                        side: BorderSide(color: Colors.orange, width: 1.0),
+                        //backgroundColor: Colors.orange,
+                        //shape: OutlinedBorder(side: BorderSide(style: BorderStyle.solid))
+                      ),
+                      child: Text(
+                          index + 1 == questions.length
+                              ? "See result"
+                              : "Next Question",
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
